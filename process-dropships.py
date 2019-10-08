2dad5f58de8a29e03f2aefa872f2ae8a5c9a0ced
Sam Rudd <samdrudd@gmail.com>
2019-10-08 01:07:50 -0700

    account for multiple tracking numbers returned from TAW (issue #2)

--- process-dropships.py
+++ process-dropships.py
@@ -166,49 +166,63 @@ def get_tracking():
             # PARSE TRACKING INFO FROM TAW RESPONSE
             root = ET.ElementTree(ET.fromstring(r.content)).getroot()
 
-            for record in root.findall('Record'):
-                log(record.find('TrackNum').text.strip())
-
-            exit()
+            records = root.findall('Record')
+            if len(records) < 1:
+                log(f"No tracking info returned for {PONumber}, skipping...")
+                continue
 
             log(f"[{PONumber}] Tracking info found! Parsing...")
 
-            data = {}
+            i = 1
+            for record in root.findall('Record'):
+                # For the first record, actually add the tracking number as shipping info
+                if i == 1:
+                    data = {}
 
-            order_date_str = record.find('OrderDate').text
-            order_date_obj = datetime.datetime.strptime(order_date_str, '%m/%d/%Y')
-            order_date_str = order_date_obj.strftime('%Y-%m-%dT%H:%M:%S.000Z')
+                    order_date_str = record.find('OrderDate').text
+                    order_date_obj = datetime.datetime.strptime(order_date_str, '%m/%d/%Y')
+                    order_date_str = order_date_obj.strftime('%Y-%m-%dT%H:%M:%S.000Z')
 
-            data['ship_date'] = order_date_str
+                    data['ship_date'] = order_date_str
 
-            log(f"[{PONumber}] Ship date: {data['ship_date']}")
+                    log(f"[{PONumber}] Ship date: {data['ship_date']}")
 
-            data['tracking_number'] = record.find('TrackNum').text.strip()
+                    data['tracking_number'] = record.find('TrackNum').text.strip()
 
-            log(f"[{PONumber}] Tracking number: {data['tracking_number']}")
+                    log(f"[{PONumber}] Tracking number: {data['tracking_number']}")
 
-            # IF NO TRACKING NUMBER, LOG IT AND GO ON TO THE NEXT ONE
-            if data['tracking_number'] == "":
-                log(f"[{PONumber}] No tracking number found. Skipping.")
-                continue
+                    # IF NO TRACKING NUMBER, LOG IT AND GO ON TO THE NEXT ONE
+                    if data['tracking_number'] == "":
+                        log(f"[{PONumber}] No tracking number found. Skipping.")
+                        continue
 
-            data['carrier_name'] = record.find('Type').text.strip()
+                    data['carrier_name'] = record.find('Type').text.strip()
 
-            # IF NO VENDOR, LOG IT AND GO ON TO THE NEXT ONE
-            if data['carrier_name'] == "":
-                log(f"[{PONumber}] No vendor found. Skipping.")
-                continue
+                    # IF NO VENDOR, LOG IT AND GO ON TO THE NEXT ONE
+                    if data['carrier_name'] == "":
+                        log(f"[{PONumber}] No vendor found. Skipping.")
+                        continue
 
-            data['shipping_method'] = "ground"
-            data['cost'] = 14
+                    data['shipping_method'] = "ground"
+                    data['cost'] = 14
 
-            log(f"[{PONumber}] Vendor: {data['carrier_name']}")
-            log(f"[{PONumber}] Sending to ordoro...")
+                    log(f"[{PONumber}] Vendor: {data['carrier_name']}")
+                    log(f"[{PONumber}] Sending to ordoro...")
 
-            # SEND TRACKING INFO TO ORDORO
-            r = ordoro.post_shipping_info(PONumber, data)
+                    # SEND TRACKING INFO TO ORDORO
+                    r = ordoro.post_shipping_info(PONumber, data)
 
-            log(f"[{PONumber}] Response from ordoro:\n\r{r.content.decode('UTF-8')}")
+                    log(f"[{PONumber}] Response from ordoro:\n\r{r.content.decode('UTF-8')}")
+                else:
+                    taw_invoice_num = record.find('InvoiceNumber').text.strip()
+                    tracking_number = record.find('TrackNum').text.strip()
+                    ordoro.post_comment(
+                        PONumber,
+                        f'Additional tracking information: '
+                        f'\n\rTAW Order ID: {taw_invoice_num}'
+                        f'\n\rTracking Number: {tracking_number}')
+                i = i + 1
+
             log(f"[{PONumber}] Removing 'Awaiting Tracking' tag...")
 
             # DELETE AWAITING TRACKING TAG FROM ORDER
